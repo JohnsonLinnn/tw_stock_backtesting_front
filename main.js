b231436491ad46fc518e03cf8728c2d2a6f3fa9b
林展丞 <johnson@linzhanchengs-MacBook-Pro.local>
2021-05-15 22:15:26 +0800

    finished syncing firestore

---
+++ main.js
@@ -0,0 +1,16 @@
+console.log('you are good to go')
+function myFunction() {
+    document.getElementById("demo").innerHTML = "This is johnson";
+  }
+var db = firebase.firestore();
+var docRef = db.collection("2603.TW").doc("Close");
+docRef.get().then((doc) => {
+    if (doc.exists) {
+        console.log("Document data:", doc.data());
+    } else {
+        // doc.data() will be undefined in this case
+        console.log("No such document!");
+    }
+}).catch((error) => {
+    console.log("Error getting document:", error);
+});
